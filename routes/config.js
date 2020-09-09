685d906795ad6735149acc695a9bec746ee5642c
Isaiah Harrison <ijh5005@outlook.com>
2020-09-08 23:03:58 -0400

    config deleted

--- routes/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "SQ_SANDBOX_BASEURL" : "https://connect.squareupsandbox.com",
-    dashboard: {
-        "SQ_SANDBOX_APP_ID" : "sq0idp-4CVw5fpKwLHOxXyqa1LoZQ",
-        "SQ_SANDBOX_APP_SECRET" : "EAAAECYO2279a_jgBqnzmXPGE19IouASfJ9WAa0Ub_M4luqrT4Ijd01XYyP-mQ7v"
-    }
-}

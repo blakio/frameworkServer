b0633c96d13f1a83f326f7936572706305791278
Isaiah Harrison <ijh5005@outlook.com>
2020-09-13 09:19:59 -0400

    debugging

--- server.js
+++ server.js
@@ -6,12 +6,11 @@ require('dotenv').config()
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-const server = require('http').createServer(app);
-const options = { /* ... */ };
-const io = require('socket.io')(server, options);
-io.on('connection', socket => {
-    require("./routes")(app, socket);
-});
+// const server = require('http').createServer(app);
+// const options = { /* ... */ };
+// const io = require('socket.io')(server, options);
+// io.on('connection', socket => {
+// });
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
@@ -19,5 +18,7 @@ app.use(express.json());
 app.use(cors());
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
+require("./routes")(app, socket);
 
-server.listen(PORT, () => console.log(`App running on port ${PORT}!`));
+app.listen(PORT, () => console.log(`App running on port ${PORT}!`));
+// server.listen(PORT, () => console.log(`App running on port ${PORT}!`));
